eaaed1a6fa7c126eea09fa2aa8893944ae1045fb
Pravali Uppugunduri <46845440+pravali96@users.noreply.github.com>
2021-08-02 23:18:44 -0700

    Add files via upload

---
+++ Stock Price Trend Analysis using News Paper Headlines/NaiveBayes.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 21 13:31:17 2021
+
+@author: prava
+"""
+import pandas as pd
+df = pd.read_csv('C:/Users/prava/StockSenti/Stock_Data.csv', encoding = "ISO-8859-1")
+
+train = df[df['Date'] < '20150101']
+test = df[df['Date'] > '20141231']
+
+# Removing punctuations
+data=train.iloc[:,2:27]
+data.replace("[^a-zA-Z]"," ",regex=True, inplace=True)
+
+# Renaming column names for ease of access
+list1= [i for i in range(25)]
+new_Index=[str(i) for i in list1]
+data.columns= new_Index
+data.head(5)
+
+# Convertng headlines to lower case
+for index in new_Index:
+    data[index]=data[index].str.lower()
+data.head(1)
+
+# aConverting rows to vectors
+' '.join(str(x) for x in data.iloc[1,0:25])
+
+headlines = []
+for row in range(0,len(data.index)):
+    headlines.append(' '.join(str(x) for x in data.iloc[row,0:25]))
+    
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+# implement BAG OF WORDS
+countvector=CountVectorizer(ngram_range=(2,2))
+traindataset=countvector.fit_transform(headlines)
+
+# implement Niave Bayes Classifier using CountVectorizer
+naive=MultinomialNB()
+naive.fit(traindataset,train['Label'])
+
+## Predict for the Test Dataset
+test_transform= []
+for row in range(0,len(test.index)):
+    test_transform.append(' '.join(str(x) for x in test.iloc[row,2:27]))
+test_dataset = countvector.transform(test_transform)
+predictions = naive.predict(test_dataset)
+
+# Import library to check accuracy
+from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
+
+matrix=confusion_matrix(test['Label'],predictions)
+print(matrix)
+score=accuracy_score(test['Label'],predictions)
+print(score)
+report=classification_report(test['Label'],predictions)
+print(report)
+
+## Received an accuracy of 84.6
+
+# Using TfIDVectoizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# implement BAG OF WORDS
+tfidvector=TfidfVectorizer(ngram_range=(2,2))
+traindataset=tfidvector.fit_transform(headlines)
+
+# implement Niave Bayes Classifier using TfIDVectorizer
+naive=MultinomialNB()
+naive.fit(traindataset,train['Label'])
+
+## Predict for the Test Dataset
+test_transform= []
+for row in range(0,len(test.index)):
+    test_transform.append(' '.join(str(x) for x in test.iloc[row,2:27]))
+test_dataset = tfidvector.transform(test_transform)
+predictions = naive.predict(test_dataset)
+
+# Import library to check accuracy
+from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
+
+matrix=confusion_matrix(test['Label'],predictions)
+print(matrix)
+score=accuracy_score(test['Label'],predictions)
+print(score)
+report=classification_report(test['Label'],predictions)
+print(report)
+
+## Received an accuracy of 85
